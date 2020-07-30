f487cd40af31be0c876a92544b723c323acef236
Rafael Cotta <rcotta@gmail.com>
2020-07-30 01:34:45 -0300

    Initial Spring Initialzr version

---
+++ src/main/java/com/example/couponmanager/CouponmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.couponmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponmanagerApplication.class, args);
+	}
+
+}
